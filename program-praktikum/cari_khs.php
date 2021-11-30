811a90cfa9304e9d24d9a4a9f1c3095ee2545d91
aldi.arpateam <aldi1900018037@webmail.uad.ac.id>
2021-11-30 18:14:46 +0700

    Mengupload MODIFIKASI Program Praktikum ke-7

--- program-praktikum/cari_khs.php
+++ program-praktikum/cari_khs.php
@@ -20,17 +20,26 @@
 		<tr>
 			<th>No</th>
 			<th>NIM</th>
+			<th>Nama Mahasiswa</th>
 			<th>Kode MK</th>
+			<th>Nama Mata Kuliah</th>
 			<th>Nilai</th>
 		</tr>
 
 		<?php
 			if(isset($_GET['cari'])){
-				$cari = $_GET['cari'];
-				$sql="select * from KHS where nim = ' ".$cari." '";
+				$cari 	= $_GET['cari'];
+				$sql 	= "SELECT
+							mahasiswa.NIM, nilai, mahasiswa.nama AS namaMahasiswa, kode, matakuliah.nama AS namaMK FROM khs
+							INNER JOIN mahasiswa ON khs.NIM=mahasiswa.nim
+							INNER JOIN matakuliah ON khs.kodeMK=matakuliah.kode
+							WHERE mahasiswa.NIM='$cari'";
 				$tampil = mysqli_query($con,$sql);
 			}else{
-				$sql="select * from KHS";
+				$sql 	= "SELECT
+							mahasiswa.NIM, nilai, mahasiswa.nama AS namaMahasiswa, kode, matakuliah.nama AS namaMK FROM khs
+							INNER JOIN mahasiswa ON khs.NIM=mahasiswa.nim
+							INNER JOIN matakuliah ON khs.kodeMK=matakuliah.kode";
 				$tampil = mysqli_query($con,$sql);
 			}
 
@@ -41,7 +50,9 @@
 		<tr>
 			<td><?php echo $no++; ?></td>
 			<td><?php echo $r['NIM']; ?></td>
-			<td><?php echo $r['kodeMK']; ?></td>
+			<td><?php echo $r['namaMahasiswa']; ?></td>
+			<td><?php echo $r['kode']; ?></td>
+			<td><?php echo $r['namaMK']; ?></td>
 			<td><?php echo $r['nilai']; ?></td>
 		</tr>
 
